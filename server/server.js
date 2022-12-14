0e2d10103612b1ecc698f751787ae0d175a3ab94
Koreyton Evans <koreyton.evans1995@gmail.com>
2022-12-14 15:51:25 -0600

    latest commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// Static files = not handled by the server
+
+// Express. static is middleware, makes it so that any request has access to the given file
+app.use(express.static('public'));
+//app.use(express.static($(__dirname}/public ')) //gets directory when you don't know what it's called
+
+//Gets the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+app.listen(4000, () => console.log(`Running on 4000!`));
